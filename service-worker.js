3f8769b7a66691641ca4618f32b77abdefcebe0f
richsemic13 <joca.semic.ui@phinmaed.com>
2026-03-05 05:27:25 +0800

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,34 @@
-const CACHE_NAME = "todo-pwa-v1";
+const CACHE_NAME = "todo-pwa-v2";
 const FILES = [
   "./",
   "./index.html",
   "./app.js",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", e => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
   );
 });
 
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME)
+            .map(k => caches.delete(k))
+      )
+    )
+  );
+});
+
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request)
+      .then(res => res || fetch(e.request))
   );
 });
