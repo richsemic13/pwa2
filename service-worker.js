05f06c1804dc78083869b15a545ccec6ad21fce8
richsemic13 <joca.semic.ui@phinmaed.com>
2026-03-05 21:39:45 +0800

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,32 +3,17 @@ const FILES = [
   "./",
   "./index.html",
   "./app.js",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+  "./manifest.json"
 ];
 
 self.addEventListener("install", e => {
-  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
   );
 });
 
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME)
-            .map(k => caches.delete(k))
-      )
-    )
-  );
-});
-
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request)
-      .then(res => res || fetch(e.request))
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
